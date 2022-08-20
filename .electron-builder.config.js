c3a6f9923bd5bf8b805c9d3da6b9887cdf993de7
AyrtonAndino <ayrton.andino@outlook.com>
2022-08-19 21:29:17 -0300

    revert changes to .electron-builder.config.js

--- .electron-builder.config.js
+++ .electron-builder.config.js
@@ -1,15 +1,7 @@
 if (process.env.VITE_APP_VERSION === undefined) {
     const now = new Date()
 
-    const year = String(now.getUTCFullYear() - 2000)
-
-    const month = String(now.getUTCMonth() + 1).padStart(2, '0')
-
-    const day = String(now.getUTCDate()).padStart(2, '0')
-
-    const stamp = String(now.getUTCHours() * 60 + now.getUTCMinutes())
-
-    process.env.VITE_APP_VERSION = `${year}${month}.${day}-${stamp}`
+    process.env.VITE_APP_VERSION = `${now.getUTCFullYear() - 2000}.${now.getUTCMonth() + 1}.${now.getUTCDate()}-${now.getUTCHours() * 60 + now.getUTCMinutes()}`
 }
 
 /**
