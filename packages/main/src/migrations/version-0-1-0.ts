5eff671c3e05e90cdfe1642b89572a637117027f
AyrtonAndino <ayrton.andino@outlook.com>
2022-08-19 21:16:56 -0300

    fix Object type 'unknown' error
     on migrations.ts

--- packages/main/src/migrations/version-0-1-0.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    '0.1.0': (store: App.ElectronStore): void => {
-        store.set('printerUrl', 'tcp://127.0.0.1')
-        store.set('printerPort', 9100)
-        store.set('printerModel', 'EPSON')
-    },
-}
