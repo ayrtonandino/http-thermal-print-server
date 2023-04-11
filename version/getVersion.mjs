274db7e3041654c3c5a14fec21bdffe816311d8b
AyrtonAndino <ayrton.andino@outlook.com>
2023-04-11 19:23:51 -0300

    Update getVersion.mjs

--- version/getVersion.mjs
+++ version/getVersion.mjs
@@ -1,6 +1,5 @@
 /**
  * Entry function for get app version.
- * In current implementation, it returns `version` from `package.json`, but you can implement any logic here.
  * Runs several times for each vite configs and electron-builder config.
  * @return {string}
  */
